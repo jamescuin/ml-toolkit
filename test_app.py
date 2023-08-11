3b82546c269296e50ff15b129141ac8c8c8aafa8
jcuin <james.cuin@sensestreet.com>
2023-08-11 09:37:23 +0000

    Add test of streamlit

---
+++ test_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+st.title("Test App")
+st.header("Header")
+st.markdown("Markdown")
+st.subheader("Subheader")
+st.caption("Caption")
+st.code("x=42")
+st.latex(r''' a+a r^1+a r^2+a r^3 ''')
